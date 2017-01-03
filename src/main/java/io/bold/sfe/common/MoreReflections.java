b370e71169510dc9bdddb238e924a29c2e46f833
David Byttow <david.byttow@gmail.com>
2017-01-02 17:20:00 -0800

    Adds reflection method for component types

--- src/main/java/io/bold/sfe/common/MoreReflections.java
+++ src/main/java/io/bold/sfe/common/MoreReflections.java
@@ -5,6 +5,10 @@ import org.reflections.Reflections;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.Set;
 
 public final class MoreReflections {
@@ -12,6 +16,22 @@ public final class MoreReflections {
   public static Set<Class<?>> getTypesAnnotatedWith(String packagePrefix, Class<? extends Annotation> annotation) {
     Reflections reflections = new Reflections(packagePrefix);
     return reflections.getTypesAnnotatedWith(annotation);
+  }
+
+  public static Class<?> getFieldComponentType(Field field) {
+    Class<?> type = field.getType();
+    if (type.isArray()) {
+      return type.getComponentType();
+    }
+
+    if (Collection.class.isAssignableFrom(type)) {
+      ParameterizedType pt = (ParameterizedType) field.getGenericType();
+      Type[] pts = pt.getActualTypeArguments();
+      if (pts.length == 1) {
+        return (Class<?>)pts[0];
+      }
+    }
+    return null;
   }
 
   public static <T> T forceNewInstance(Class<T> type) {
---
+++ src/test/java/io/bold/sfe/common/MoreReflectionsTest.java
@@ -0,0 +1,28 @@
+package io.bold.sfe.common;
+
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class MoreReflectionsTest {
+
+  static class Foo {
+    List<Integer> list;
+    Set<Integer> set;
+    Collection<Integer> collection;
+    Integer[] array;
+    boolean bool;
+  }
+
+  @Test public void testGenerics() throws NoSuchFieldException {
+    assertThat(MoreReflections.getFieldComponentType(Foo.class.getDeclaredField("list"))).isEqualTo(Integer.class);
+    assertThat(MoreReflections.getFieldComponentType(Foo.class.getDeclaredField("set"))).isEqualTo(Integer.class);
+    assertThat(MoreReflections.getFieldComponentType(Foo.class.getDeclaredField("collection"))).isEqualTo(Integer.class);
+    assertThat(MoreReflections.getFieldComponentType(Foo.class.getDeclaredField("array"))).isEqualTo(Integer.class);
+    assertThat(MoreReflections.getFieldComponentType(Foo.class.getDeclaredField("bool"))).isNull();
+  }
+}
