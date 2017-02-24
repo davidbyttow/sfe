3c77a88244d247a85c4bfe7928d1ba1df72d3ec5
David Byttow <david.byttow@gmail.com>
2017-02-23 23:54:53 -0800

    No need for container request right now

--- src/main/java/com/simplethingsllc/framework/http/error/ErrorMediaConverter.java
+++ src/main/java/com/simplethingsllc/framework/http/error/ErrorMediaConverter.java
@@ -1,53 +1,16 @@
 package com.simplethingsllc.framework.http.error;
 
-import com.google.common.base.Strings;
-import org.glassfish.jersey.server.ContainerRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
-import javax.inject.Provider;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 class ErrorMediaConverter {
   private static final Logger log = LoggerFactory.getLogger(ErrorMediaConverter.class);
 
-  private final Provider<ContainerRequest> containerRequestProvider;
-
-  @Inject ErrorMediaConverter(Provider<ContainerRequest> containerRequestProvider) {
-    this.containerRequestProvider = containerRequestProvider;
-  }
-
   Response convert(ErrorDetails errorDetails) {
-    ContainerRequest containerRequest = containerRequestProvider.get();
-    String subType = "";
-    if (containerRequest != null) {
-      MediaType mediaType = containerRequest.getMediaType();
-      if (mediaType != null) {
-        subType = mediaType.getSubtype();
-      }
-      if (Strings.isNullOrEmpty(subType)) {
-        if (containerRequest.getMethod().equals("GET")) {
-          subType = "html";
-        }
-      }
-    }
-
-    switch (subType) {
-      case "json":
-        return Response.status(errorDetails.status)
-          .entity(errorDetails)
-          .build();
-      case "html":
-        // TODO(d): Handle this differently
-        return Response.status(errorDetails.status)
-          .entity(errorDetails)
-          .build();
-      default:
-        return Response.status(errorDetails.status)
-          .entity(errorDetails)
-          .build();
-    }
+    return Response.status(errorDetails.status)
+      .entity(errorDetails)
+      .build();
   }
 }
