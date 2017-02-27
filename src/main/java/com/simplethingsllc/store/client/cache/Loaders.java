efeda47f0e4db19609edaeb97110cc2390ceef9e
David Byttow <david.byttow@gmail.com>
2017-02-27 15:31:40 -0800

    Makes loader availalbe

--- src/main/java/com/simplethingsllc/store/client/cache/Loaders.java
+++ src/main/java/com/simplethingsllc/store/client/cache/Loaders.java
@@ -8,7 +8,7 @@ import javax.annotation.Nullable;
 
 public final class Loaders {
 
-  static <T> CacheLoader<String, T> newIdLoader(EntityStoreAsync entityStore, Class<T> type) {
+  public static <T> CacheLoader<String, T> newIdLoader(EntityStoreAsync entityStore, Class<T> type) {
     return new CacheLoader<String, T>() {
       @Nullable @Override public T load(String key) {
         return entityStore.get(key, type);
@@ -16,7 +16,7 @@ public final class Loaders {
     };
   }
 
-  static <T> CacheLoader<String, T> newSecondaryIdLoader(EntityStoreAsync entityStore, String fieldName, Class<T> type) {
+  public static <T> CacheLoader<String, T> newSecondaryIdLoader(EntityStoreAsync entityStore, String fieldName, Class<T> type) {
     return new CacheLoader<String, T>() {
       @Nullable @Override public T load(String key) {
         return Queries.fetchBySecondaryId(entityStore, fieldName, key, type);
